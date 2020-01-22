edaf19292e1e7613591f99115dafda1cae5dfc82
Dennis Njoroge <dennis.netcraft@gmail.com>
2020-01-22 09:42:51 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,8 +24,8 @@ const Index = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
   return (
     <Layout>
-      <Helmet title={'Home Page'} />
-      <Header title="Home Page">Gatsby Tutorial Starter</Header>
+      <Helmet title={'The Mode'} />
+      <Header title="The Mode">Gatsby Tutorial Starter</Header>
       <PostWrapper>
         {edges.map(({ node }) => {
           const { id, excerpt, frontmatter } = node;
